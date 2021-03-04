61bc9a358985930705d82d5f237c94cd3c03cc40
Danielo06 <d.santamaria@uniandes.edu.co>
2021-03-04 12:17:09 -0500

    cositas varias

--- consola.py
+++ consola.py
@@ -1,6 +1,11 @@
 import busqueda_binaria as raiz
 
 if __name__ == '__main__':
+    """
+    Aquí corre la consola
+    SOlo se llama al método raiz de la clase busqueda bianria
+    se pregunta si quiere hallar o no una raiz
+    """
     print('Hola!')
     print('Quieres saber la raiz de algún numero?')
     print('1. Si')
