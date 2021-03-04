71db60ed085b81b6113c675a15af9a4f49547445
Danielo06 <d.santamaria@uniandes.edu.co>
2021-03-04 10:08:26 -0500

    EJercicios de if

---
+++ ejercicios.py
@@ -0,0 +1,14 @@
+
+#repaso de if
+nombre_1 = input('Hola! Como te llamas? ')
+edad_1 = int(input(f'Cual es tu edad, {nombre_1}? '))
+
+nombre_2 = input('y como se llama tu compañer@? ')
+edad_2 = int(input(f'Cual es tu edad, {nombre_2}? '))
+
+if edad_1 == edad_2:
+    print('Wow, los dos tienen la misma edad!')
+elif edad_1 > edad_2:
+    print(f'{nombre_1} es mayor que {nombre_2}')
+else:
+    print(f'{nombre_2} es mayor que {nombre_1}')
