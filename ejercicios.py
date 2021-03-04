b540727146747529bab4fa4c603a4393c8f71674
Danielo06 <d.santamaria@uniandes.edu.co>
2021-03-04 10:14:59 -0500

    Ejerciciso de iteración

--- ejercicios.py
+++ ejercicios.py
@@ -1,14 +1,22 @@
 
 #repaso de if
-nombre_1 = input('Hola! Como te llamas? ')
-edad_1 = int(input(f'Cual es tu edad, {nombre_1}? '))
 
-nombre_2 = input('y como se llama tu compañer@? ')
-edad_2 = int(input(f'Cual es tu edad, {nombre_2}? '))
 
-if edad_1 == edad_2:
-    print('Wow, los dos tienen la misma edad!')
-elif edad_1 > edad_2:
-    print(f'{nombre_1} es mayor que {nombre_2}')
-else:
-    print(f'{nombre_2} es mayor que {nombre_1}')
+def operaciones_id():
+    if edad_1 == edad_2:
+        print('Wow, los dos tienen la misma edad!')
+    elif edad_1 > edad_2:
+        print(f'{nombre_1} es mayor que {nombre_2}')
+    else:
+        print(f'{nombre_2} es mayor que {nombre_1}')
+
+
+
+if __name__ == '__main__':
+    nombre_1 = input('Hola! Como te llamas? ')
+    edad_1 = int(input(f'Cual es tu edad, {nombre_1}? '))
+
+    nombre_2 = input('y como se llama tu compañer@? ')
+    edad_2 = int(input(f'Cual es tu edad, {nombre_2}? '))
+
+
