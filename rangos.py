fb33a16b1e4737cfac13db1e0227432054031a54
Danielo06 <d.santamaria@uniandes.edu.co>
2021-03-04 21:25:04 -0500

    range

---
+++ rangos.py
@@ -0,0 +1,12 @@
+
+def impares_menores_a_cien():
+    """
+    imprime todos los números impares de 0 a 100
+    """
+    rango = range(1,100,2)
+
+    for i in rango:
+        print(i)
+
+if __name__ == '__main__':
+    impares_menores_a_cien()
